11aa65b4cfbfb0246cea2bc198213978f17dcdb5
Vincent Tran <vincenttran197120@gmail.com>
2020-11-07 22:02:31 -0600

    Added login redirection

--- frontend/services/loginService.js
+++ frontend/services/loginService.js
@@ -1,5 +1,25 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '{APP-ID}',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v8.0'
+  });
+  FB.AppEvents.logPageView();   
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
 function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
+  FB.getLoginStatus(function(response) {
+    if(response.status === "connected") {
+      window.location.replace("https://dba15a5f3002.ngrok.io/frontend/pages/mainScreen.html"); //Replace w/ link later
+    };
+  });
+}
